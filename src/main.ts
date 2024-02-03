993218e921f2e9c4256dcfeb2f21fd2ec245a61d
Oshimia <120189186+Oshimia@users.noreply.github.com>
2024-02-03 19:38:46 +0700

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,62 @@
 import { Firebot } from "@crowbartools/firebot-custom-scripts-types";
 
 interface Params {
-  message: string;
+	message: string;
 }
 
 const script: Firebot.CustomScript<Params> = {
-  getScriptManifest: () => {
-    return {
-      name: "Starter Custom Script",
-      description: "A starter custom script for build",
-      author: "SomeDev",
-      version: "1.0",
-      firebotVersion: "5",
-    };
-  },
-  getDefaultParameters: () => {
-    return {
-      message: {
-        type: "string",
-        default: "Hello World!",
-        description: "Message",
-        secondaryDescription: "Enter a message here",
-      },
-    };
-  },
-  run: (runRequest) => {
-    const { logger } = runRequest.modules;
-    logger.info(runRequest.parameters.message);
-  },
+	getScriptManifest: () => {
+		return {
+			name: "levenshteinDistance",
+			description: "calculates the Levenshtein distance between two strings",
+			author: "Oshimia",
+			version: "1.0",
+			firebotVersion: "5",
+			startupOnly: true,
+		};
+	},
+	getDefaultParameters: null,
+	run: (runRequest) => {
+		const { replaceVariableManager } = runRequest.modules;
+
+		replaceVariableManager.registerReplaceVariable({
+			definition: {
+				handle: "levenshteinDistance",
+				description: "Takes in two strings and tells you how many single digit changes it would take to turn one into the other.",
+				usage: "levenshteinDistance[arg1, arg2]",
+				examples: [
+					{
+						usage: "levenshteinDistance[this, that]",
+						description: "expected output of 2, the number of changes to turn one string into another"
+					}
+				],
+				possibleDataOutput: ["number"],
+			},
+			evaluator: (_, s1, s2) => {
+				const m = s1.length;
+				const n = s2.length;
+				const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+				for (let i = 0; i <= m; i++) {
+					for (let j = 0; j <= n; j++) {
+						if (i === 0) {
+							dp[i][j] = j;
+						} else if (j === 0) {
+							dp[i][j] = i;
+						} else {
+							dp[i][j] = Math.min(
+								dp[i - 1][j - 1] + (s1[i - 1] === s2[j - 1] ? 0 : 1),
+								dp[i - 1][j] + 1,
+								dp[i][j - 1] + 1
+							);
+						}
+					}
+				}
+
+				return dp[m][n];
+			}
+		});
+	},
 };
 
 export default script;
